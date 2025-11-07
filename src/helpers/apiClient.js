37fcac487ea812eb22317c7be4a4559f2e72a7fd
Fabricio Hernández <fabriciofabras@gmail.com>
2025-11-07 17:32:05 -0600

    Update endpoints

--- src/helpers/apiClient.js
+++ src/helpers/apiClient.js
@@ -1,8 +1,8 @@
 // src/api/apiClient.js
 import axios from "axios";
 
-//const API_BASE_URL = import.meta.env.VITE_API_URL || "https://servirapid-server.vercel.app";
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://servirapid-server.vercel.app";
+//const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
