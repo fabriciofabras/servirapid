e3cd5fd3cc951507f7c33d2706d9cec879ab574a
Fabricio Hernández <fabriciofabras@gmail.com>
2025-11-17 17:06:34 -0600

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import ServiceOrders from './pages/ServiceOrders';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Home from "./components/Home";
 
 export default function App() {
   return (
     <div>
       <Header />
-      {/*       <Sidebar />
+{/*       <Sidebar />
  */}      <main className="p-4 mt-20">
         <h1 className="text-xl font-bold mb-4">Órdenes de servicio</h1>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <ServiceOrders />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
+        <ServiceOrders />
       </main>
     </div>
   );
